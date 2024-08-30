ebb6ab3a3644ba349317183015e701db66a08250
Adedamola Sode <adedamola.sode@gmail.com>
2024-08-30 11:20:52 +0100

    Added some more work regarding matrices

--- FOGED/src/vector.cpp
+++ FOGED/src/vector.cpp
@@ -32,29 +32,81 @@ struct Vector3D{
         return (*this);
     }
 
-    inline Vector3D operator *(const Vector3D& v, float s){
-        return (Vector3D(v.x*s, v.y*s, v.z*s));
+    Vector3D& operator +=(Vector3D& v){
+        x+=v.x;
+        y+=v.y;
+        z+=v.z;
+        return (*this);
     }
 
-    inline Vector3D operator /(const Vector3D& v, float s){
-        s = 1.0F/s;
-        return (Vector3D(v.x*s, v.y*s, v.z*s));
+    Vector3D& operator -=(Vector3D& v){
+        x-=v.x;
+        y-=v.y;
+        z-=v.z;
+        return (*this);
     }
-
-    inline Vector3D operator -(const Vector3D& v){
-        return (Vector3D(-v.x, -v.y, -v.z));
-    }
-
-    inline float Megnitude(const Vector3D& v){
-        return (sqrt(v.x*v.x+v.y*v.y+v.z*v.z));
-    }
-
 };
 
-
-int main(){
-    Vector3D v(1.0f, 2.0f, 3.0f);
-    v[0] = 4.0f; // This sets x to 4.0
-    float y_value = v[1]; // This retrieves the value of y (2.0)
-    return 0;
+inline Vector3D operator *(const Vector3D& v, float s){
+    return (Vector3D(v.x*s, v.y*s, v.z*s));
 }
+
+inline Vector3D operator /(const Vector3D& v, float s){
+    s = 1.0F/s;
+    return (Vector3D(v.x*s, v.y*s, v.z*s));
+}
+
+inline Vector3D operator -(const Vector3D& v){
+    return (Vector3D(-v.x, -v.y, -v.z));
+}
+
+inline float magnitude(const Vector3D& v){
+    return (sqrt(v.x*v.x+v.y*v.y+v.z*v.z));
+}
+
+inline Vector3D normalize(const Vector3D& v){
+    return (v/magnitude(v));
+}
+
+inline Vector3D operator +(const Vector3D& a, const Vector3D& b){
+    return(Vector3D(a.x+b.x, a.y+b.y, a.z+b.z));
+}
+
+inline Vector3D operator -(const Vector3D& a, const Vector3D& b){
+    return (Vector3D(a.x-b.x, a.y-b.y, a.z-b.z));
+}
+
+
+struct Matrix3D{
+    private:
+        float n[3][3];
+
+    public:
+        Matrix3D() = default;
+
+        Matrix3D(float n00, float n01, float n02,
+                float n10, float n11, float n12,
+                float n20, float n21, float n22){
+                    n[0][0] = n00; n[0][1] = n01; n[0][2] = n02;
+                    n[1][0] = n10; n[1][1] = n11; n[1][2] = n12;
+                    n[2][0] = n20; n[2][1] = n21; n[2][2] = n22;
+                }
+
+        Matrix3D(Vector3D& a, Vector3D& b, Vector3D& c){
+            n[0][0] = a.x; n[0][1] = a.y; n[0][2] = a.z;
+            n[1][0] = b.x; n[1][1] = b.y; n[1][2] = b.z;
+            n[2][0] = c.x; n[2][1] = c.y; n[2][2] = c.z;
+        }
+
+        float& operator ()(int i, int j){
+            return (n[j][i]);
+        }
+
+        const float& operator ()(int i, int j) const{
+            return (n[j][i]);
+        }
+
+        Vector3D& operator [](int j){
+            return (*reinterpret_cast<Vector3D *>(n[j]));
+        }
+};
---
+++ FOGED/src/vector.h
@@ -0,0 +1,10 @@
+#ifndef VECTOR3D_H
+#define VECTOR3D_H
+
+struct Vector3D{
+
+};
+
+
+
+#endif
